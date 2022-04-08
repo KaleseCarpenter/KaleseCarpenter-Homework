f2e46b86299ae8044c6c69a11e4a02045b9a4aaf
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-04-08 08:52:06 -0400

    This is my first stage of Late Night Vegan site

---
+++ Week 1 Homework/4-7-22 LNV HW/script.js
@@ -0,0 +1,44 @@
+
+
+const drinkingAge = 21;
+
+// less than or equal operator
+console.log(drinkingAge <= 3); // false
+console.log(drinkingAge <= 2); // false
+
+
+
+
+const age = 55;
+
+// greater than or equal operator. The age 55 is greater than 
+console.log(age >= 24); //true
+
+
+
+//  FOR LOOP
+// initial
+
+//   Give "i" behavior, 
+// I start with $20 and my goal is to make $200. If all my supporters pay $1 to watch my webinar. "++"" means added by 1. Then I will add 1 until I hit my goal of $200
+for( i = 20; i < 200 ; i++){
+    console.log("Thanks For The Donation", i)
+    
+  }
+
+
+  //Grading system example - I want to learn how to apply this to an online shopping cart experience. 
+
+  function testGrade(a) {
+    let result;
+    if (a > 69) {
+      result = 'Congrats, You passed!';
+    } else {
+      result = 'failed';
+    }
+    return result;
+  }
+  
+  console.log(testGrade(95));
+  // expected output: "Congrats, You passed!"
+  
