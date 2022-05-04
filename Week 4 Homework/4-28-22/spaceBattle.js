7eeb263d4624f034f0726de0c31d2c2488bb98cd
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-05-03 20:57:05 -0400

    updated keyframes, css, function

--- Week 4 Homework/4-28-22/spaceBattle.js
+++ Week 4 Homework/4-28-22/spaceBattle.js
@@ -1,64 +1,102 @@
-//Our Ship
-let ussAssembly = {
-    dodge: function(){
-        //allows us to retreat
+// Earth has been attacked by a horde of aliens! You are the captain of the USS Assembly, on a mission to destroy every last alien ship.
+
+// Battle the aliens as you try to destroy them with your lasers.
+
+// There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.
+
+// Aliens vs Humans - GAME
+
+// {H}: GO First
+//  Only can Attack in Order
+
+// After DestroyedShip = option2retreat
+
+// {A}: 6 ships
+
+// ship ={
+// hull:
+// firepower:
+// accuracy:
+// }
+
+// Guile Throw https://onlinegiftools.com/images/examples-onlinegiftools/sonic-boom.gif
+
+// https://www.fightersgeneration.com/characters/charlie-sonicshell.gif
+
+// ----{  coudla done this  }
+// function Alien(lifePoints,firePower,accuracy){
+//     this.lifePoints = lifePoints
+//     this.firePower = firePower
+//     this.accuracy = accuracy
+// }
+
+let USS_HolaMundo = {
+  lifePoints: 100,
+  firepower: 10,
+  accuracy: 10,
+  attackAlien: function(){
+    let enemyPosition = prompt(" Ready To Attack? ");
+
+    if (enemyPosition === "y") {
+      let relevantTarget = document.querySelector(".counter");
+      let bomb = document.createElement("img");
+      bomb.setAttribute(
+        "src",
+        "https://i.gifer.com/origin/d7/d7ac4f38b77abe73165d85edf2cbdb9e_w200.gif"
+      );
+      bomb.style.height = "60px";
+      relevantTarget.replaceWith(bomb);
+      
+      console.log("ATTACKING!>!>!!!");
+  
+      let ship = document.querySelector(".ussr");
+      ship.classList.toggle("pOneAttack");
+      // let x = document.getElementById()
     }
+  }
+};
 
-}
+let alienOne = {
+  lifePoints: 100,
+  firepower: 10,
+  accuracy: 10,
+};
 
-//-----------------------------------------------------
+let alienTwo = {
+  lifePoints: 50,
+  firepower: 10,
+  accuracy: 5,
+};
+let alienGang = [alienOne, alienTwo];
 
-//want to start a loop to let each player go
-
-const battle = () => {
-    let playerOneScore = 100
-
-    let alienScore = 100
-
-
-
-    console.log("Battle Started")
-//start game
-//establishRoundOne
-
-    let command = prompt(" Ready to [A]ttack or [R]etreat? ")
-    if (command === "A"){
-        let target = document.querySelector('.ufo')
-
-        let explosion = document.createElement('img')
-            explosion.setAttribute('src', '')
-        // signal explosion
-
-        //decrease alien Point Value
-    }
+const showDirections = () => {
+  alert(
+    "Earth has been attacked by a horde of aliens! You are the captain of the USS HelloWorld, on a mission to destroy every last alien ship.Battle the aliens as you try to destroy them with your lasers.There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat."
+  );
+};
 
 
-    //PlayerOne
+const attackAlien = () => {
+  // targetAlien & decrease Points
+  let enemyPosition = prompt(" Ready To Attack? ");
+
+  if (enemyPosition === "yes", "Yes") {
+    //   figure out how to make the life decrease each time alien is hit
+    let relevantTarget = document.querySelector(".counter");
+    let bomb = document.createElement("img");
+    bomb.setAttribute(
+      "src",
+      "https://i.gifer.com/origin/d7/d7ac4f38b77abe73165d85edf2cbdb9e_w200.gif"
+    );
+    bomb.style.height = "70px";
+    relevantTarget.replaceWith(bomb);
+    
+    console.log("ATTACKING!>!>!!!");
 
 
-    //Player 2
-}
+    let ship = document.querySelector(".ussr");
+    ship.classList.toggle("pOneAttack");
+    // let x = document.getElementById()
+  }
+};
 
-
-
-
-let alienMob = []
-
-
-// Round => each Player gets to attack
-
-//------------------------------------------------------------------
-
-const move = () =>{
-    let icon = document.querySelector('.ship')
-    icon.classList.toggle('moveSomething')
-
-
-}
-
-//hull = hp          We are going to put attributes that correspond with the ship
-let ussAssembly = {
-    hp: 20,
-    firepower: 5,
-    accuracy: .7
-}
